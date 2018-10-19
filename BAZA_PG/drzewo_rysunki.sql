8ba714a91924ee441137a6faa9f2e3f3cc059862
studentwsb <studentwsb@MOBILE-WSB>
2018-10-19 15:55:40 +0200

    pop

--- BAZA_PG/drzewo_rysunki.sql
+++ BAZA_PG/drzewo_rysunki.sql
@@ -1,15 +1,15 @@
-CREATE TABLE [dbo].[drzewo]
+CREATE TABLE [dbo].[drzewo_rysunki]
 (
-	[dr_id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    [dr_did] BIGINT NULL, 
-    [ dr_parent_id] BIGINT NULL, 
-    [  dr_lbl_element] VARCHAR(20) NULL, 
-    [ dr_nazwa] VARCHAR(100) NULL, 
-    [ id_kontrakt] BIGINT NULL, 
-    [ id_zespol] BIGINT NULL, 
+	[id_dr_id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [di_dr_did] BIGINT NULL, 
+    [id_dr_parent_id] BIGINT NULL, 
+    [dr_lbl_element] VARCHAR(20) NULL, 
+    [dr_nazwa] VARCHAR(100) NULL, 
+    [id_kontrakt] BIGINT NULL, 
+    [id_zespol] BIGINT NULL, 
     [id_rysunki] BIGINT NULL, 
-    [ id_detale] BIGINT NULL, 
-    [  dr_tag] VARCHAR(100) NULL, 
+    [id_detale] BIGINT NULL, 
+    [dr_tag] VARCHAR(100) NULL, 
     [dr_ikona ] INT NULL, 
-    [ dr_ilszt] INT NULL
+    [dr_ilszt] INT NULL
 )
