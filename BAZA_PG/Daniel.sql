afb2f56330007d59788a3f2b3e6902e0a396dc2f
studentwsb <studentwsb@MOBILE-WSB>
2018-10-19 14:36:10 +0200

    wqe

--- BAZA_PG/Daniel.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Daniel]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [toJa] NCHAR(10) NULL, 
-    [czesc] NCHAR(10) NULL
-)
--- BAZA_PG/Igor.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Igor]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [imie] NCHAR(10) NULL, 
-    [nazwisko] NCHAR(10) NULL
-)
