8e647373c898febf6d3c75c14ad154e9e2be5bff
studentwsb <studentwsb@MOBILE-WSB>
2018-10-19 15:53:49 +0200

    :)

--- BAZA_PG/biblioteka_czesci.sql
+++ BAZA_PG/biblioteka_czesci.sql
@@ -1,4 +1,14 @@
 CREATE TABLE [dbo].[biblioteka_czesci]
 (
-	[bc_Id] BIGINT NOT NULL PRIMARY KEY IDENTITY
+	[bc_Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [br_id] BIGINT NOT NULL, 
+    [nazwa] VARCHAR(50) NOT NULL, 
+    [symbol] VARCHAR(100) NULL, 
+    [norma] VARCHAR(60) NULL, 
+    [cena] DECIMAL(10, 2) NULL, 
+    [opis] TEXT NULL, 
+    [sb_id] BIGINT NULL, 
+    [sortowanie] VARCHAR(3) NULL, 
+    [data_zmiany_ceny] DATE NULL, 
+    [zmieniajacy] VARCHAR(50) NULL
 )
